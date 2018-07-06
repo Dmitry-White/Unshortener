9513fc16fee62bba33d2c193313086094ee50df6
Dzmitry_Belaichuk <dzmitry_belaichuk@epam.com>
2018-07-06 18:15:44 +0300

    Add url watching

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 const apiURL = "http://expandurl.com/api/v1/?url=";
 
 chrome.runtime.onMessage.addListener(message => {
-    if (message.data == "expand") {
+    if (message.data == "EXPAND URL") {
         fetchAsync(message.value)
-            .then(value => chrome.runtime.sendMessage({ data: "open", value }));
+            .then(value => chrome.runtime.sendMessage({ data: "OPEN", value }));
     };
 });
 
@@ -13,4 +13,26 @@ async function fetchAsync(url) {
     let data = await response.text();
     return data;
 }
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      // Ensuring that we check host names with and without WWW
+      var currentHost = new URL(details.url).hostname;
+      var withWWW;
+      var withoutWWW;
+      if (currentHost.indexOf('www.') == -1) {
+        withoutWWW = currentHost;
+        withWWW = 'www.' + currentHost;
+      }
+      else {
+        withWWW = currentHost;
+        withoutWWW = currentHost.substring(4);
+      }
+  
+      console.log(currentHost, withWWW, withoutWWW);
+      setTimeout(function(){},1000);
+    },
+    { urls: ["*://*.bit.ly/*"] },
+    ["blocking"]
+);
 
--- popup.js
+++ popup.js
@@ -2,17 +2,17 @@ const urlForm = document.querySelector('form.url-form');
 const urlInput = document.querySelector('input.short-url');
 const loading = document.querySelector('div.sk-cube-grid');
 
-let value = "";
+let value;
 
 urlForm.addEventListener('submit', e => {
     e.preventDefault();
     value = urlInput.value;
     loading.classList.remove("invisible");
-    chrome.runtime.sendMessage({ data: "expand", value });
+    chrome.runtime.sendMessage({ data: "EXPAND URL", value });
 });
 
 chrome.runtime.onMessage.addListener(message => {
-    if (message.data == "open") {
+    if (message.data == "OPEN") {
         loading.classList.add("invisible");
         window.open(message.value);
     }
